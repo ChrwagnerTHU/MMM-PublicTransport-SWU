f65f48e7e92962ba054f9e0e5c035631c900490d
CHrwagnerTHU <chris-xbo@outlook.de>
2025-09-24 19:26:18 +0200

    Hotfixes

--- MMM-PublicTransport-SWU.js
+++ MMM-PublicTransport-SWU.js
@@ -8,158 +8,182 @@ Module.register("MMM-PublicTransport-SWU", {
 
   // Default module config.
   defaults: {
-    stopNumber: 1008, // Change this to the stop number you want to display
-    stopString: "Hauptbahnhof", // Change this to the stop name you want to display
-    limit: 10, // Change this to the number of departures you want to display
-    updateInterval: 60000, // Update the display every minute (in ms)
+    stopNumber: 1008,             // Haltestellen-Nummer
+    stopString: "Hauptbahnhof",   // Haltestellen-Name
+    limit: 10,                    // Anzahl Abfahrten
+    updateInterval: 60000,        // Update-Intervall (ms)
     showDelay: false,
     showPlattform: [],
-    timetostop: 5, // Time it takes to reach the stop (Default: 5 Minutes)
+    timetostop: 5,                // Gehzeit zur Haltestelle (Minuten)
   },
 
   // Define start sequence.
-  start: async function () {
+  start: function () {
     Log.info("Starting module: " + this.name);
-    this.departureData = []; // Initialize departure data as empty array
-    this.getDepartureData(); // Call API immediately to get initial data
-    this.scheduleUpdate(); // Schedule update interval
+    this.departureData = [];      // leeres Array initialisieren
+    this.getDepartureData();      // initial laden
+    this.scheduleUpdate();        // regelmäßige Updates
   },
 
   // Override dom generator.
   getDom: function () {
 
-    // Create main wrapper
+    // Main wrapper
     var wrapper = document.createElement("div");
     wrapper.className = "swu-departure-wrapper";
 
-    // If there is no departure data, display error message
-    if (this.departureData.length <= 0) {
+    // Falls keine Daten da sind
+    if (!this.departureData || this.departureData.length === 0) {
       wrapper.innerHTML = "Dieser Service steht aktuell nicht zur Verfügung.";
       wrapper.className = "dimmed light small";
       return wrapper;
     }
 
-    // Create header wrapper and add text
+    // Header
     var headerWrapper = document.createElement("div");
     headerWrapper.className = "swu-departure-header";
     headerWrapper.innerHTML = this.config.stopString;
 
-    // Create table wrapper
+    // Tabelle
     var tableWrapper = document.createElement("table");
     tableWrapper.className = "swu-departure-table";
     tableWrapper.style.padding = "10px";
     tableWrapper.style.textAlign = "left";
 
-    // Create table header row
+    // Tabellenkopf (fehlendes '>' war ein Bug)
     var tableHeader = document.createElement("tr");
     tableHeader.className = "swu-departure-table-header";
-    tableHeader.innerHTML = "<th style='padding: 5px;'>Linie</th><th style='padding: 5px;'>Plattform</th><th style='padding: 5px;'>Richtung</th><th style='padding: 5px;'>Abfahrt</th><th style='padding: 5px;'></th";
+    tableHeader.innerHTML =
+      "<th style='padding:5px;'>Linie</th>" +
+      "<th style='padding:5px;'>Plattform</th>" +
+      "<th style='padding:5px;'>Richtung</th>" +
+      "<th style='padding:5px;'>Abfahrt</th>" +
+      "<th style='padding:5px;'></th>";
     tableWrapper.appendChild(tableHeader);
 
     var countLines = 0;
+    var tts = Number(this.config.timetostop) || 0; // Gehzeit (Min)
 
-    // Loop through departure data and create table rows for each departure
+    // Zeilen rendern
     for (var i = 0; i < this.departureData.length; i++) {
+      if (countLines >= this.config.limit) break;
 
-      if (countLines < this.config.limit) {
+      var departure = this.departureData[i];
 
-        var departure = this.departureData[i];
+      // Plattform-Filter
+      if (
+        this.config.showPlattform.length === 0 ||
+        this.config.showPlattform.indexOf(departure.PlatformName) !== -1
+      ) {
+        var tableRow = document.createElement("tr");
+        tableRow.className = "swu-departure-table-row";
 
-        if (this.config.showPlattform.length === 0 || !(this.config.showPlattform.indexOf(departure.PlatformName) === -1)) {
+        // Linie (Piktogramm)
+        var routeNumber = document.createElement("td");
+        routeNumber.className = "swu-departure-routeNumber";
+        var img = document.createElement("img");
+        img.src = "modules/MMM-PublicTransport-SWU/lines/Linie_" + departure.RouteNumber + "_Pikto.gif";
+        img.setAttribute("width", "20");
+        routeNumber.appendChild(img);
+        tableRow.appendChild(routeNumber);
 
-          var tableRow = document.createElement("tr");
-          tableRow.className = "swu-departure-table-row";
+        // Plattform
+        var platformName = document.createElement("td");
+        platformName.className = "swu-departure-platformName";
+        platformName.style.padding = "5px";
+        platformName.innerHTML = departure.PlatformName || "";
+        tableRow.appendChild(platformName);
 
-          var routeNumber = document.createElement("td");
-          routeNumber.className = "swu-departure-routeNumber";
-          var img = document.createElement("img");
-          img.src = "modules/MMM-PublicTransport-SWU/lines/Linie_" + departure.RouteNumber + "_Pikto.gif";
-          img.setAttribute("width", "20");
-          routeNumber.appendChild(img);
-          tableRow.appendChild(routeNumber);
+        // Richtung
+        var directionText = document.createElement("td");
+        directionText.className = "swu-departure-directionText";
+        directionText.style.padding = "5px";
+        directionText.innerHTML = departure.DepartureDirectionText || "";
+        tableRow.appendChild(directionText);
 
-          var platformName = document.createElement("td");
-          platformName.className = "swu-departure-platformName";
-          platformName.style.padding = "5px";
-          platformName.innerHTML = departure.PlatformName;
-          tableRow.appendChild(platformName);
+        // Countdown
+        var countdown = document.createElement("td");
+        countdown.className = "swu-departure-countdown";
+        countdown.style.padding = "5px";
 
-          var directionText = document.createElement("td");
-          directionText.className = "swu-departure-directionText";
-          directionText.style.padding = "5px";
-          directionText.innerHTML = departure.DepartureDirectionText;
-          tableRow.appendChild(directionText);
+        var rawSeconds = Number(departure.DepartureCountdown) || 0;
+        var countdownMinutes = Math.floor(rawSeconds / 60);
 
-          var countdown = document.createElement("td");
-          countdown.className = "swu-departure-countdown";
-          countdown.style.padding = "5px";
-          var countdownMinutes = Math.floor(departure.DepartureCountdown / 60);
-          if (countdownMinutes < 1) {
-            countdown.innerHTML = "Jetzt";
+        // Text
+        countdown.innerHTML = countdownMinutes < 1 ? "Jetzt" : (countdownMinutes + " min");
+
+        // >>> Einfärbung hier beim Rendern <<<
+        if (tts > 0) {
+          if (countdownMinutes < tts - 1) {
+            // zu knapp
+            countdown.style.color = "red";
+          } else if (Math.abs(countdownMinutes - tts) <= 1) {
+            // genau im Fenster
+            countdown.style.color = "gold";
           } else {
-            countdown.innerHTML = countdownMinutes + " min";
+            // entspannt
+            countdown.style.color = "";
           }
-          tableRow.appendChild(countdown);
-
-          var delay = document.createElement("td");
-          delay.style.padding = "5px";
-          if (this.config.showDelay) {
-            var delayMinutes = Math.floor(departure.DepartureDeviation / 60);
-            if (delayMinutes > 0) {
-              var delay = document.createElement("td");
-              delay.style.padding = "5px";
-              delay.style.color = "red";
-              delay.innerHTML = "+ " + delayMinutes;
-            } else {
-              delay.innerHTML = "";
-            }
-          }
-          tableRow.appendChild(delay);
-
-          countLines = countLines + 1;
-
-          // Add table row to table wrapper
-          tableWrapper.appendChild(tableRow);
         }
-      } else {
-        break;
+        tableRow.appendChild(countdown);
+
+        // Verspätung (optional)
+        var delayCell = document.createElement("td");
+        delayCell.style.padding = "5px";
+        if (this.config.showDelay) {
+          var devSec = Number(departure.DepartureDeviation) || 0;
+          var delayMinutes = Math.floor(devSec / 60);
+          if (delayMinutes > 0) {
+            delayCell.style.color = "red";
+            delayCell.innerHTML = "+ " + delayMinutes;
+          } else {
+            delayCell.innerHTML = "";
+          }
+        }
+        tableRow.appendChild(delayCell);
+
+        countLines++;
+        tableWrapper.appendChild(tableRow);
       }
     }
-    // Append header and table to main wrapper
+
+    // Zusammenbauen
     wrapper.appendChild(headerWrapper);
     wrapper.appendChild(tableWrapper);
 
     return wrapper;
   },
 
-  // Schedule update interval
+  // Update-Intervall
   scheduleUpdate: function () {
-    var nextLoad = this.config.updateInterval;
-    if (typeof delay !== "undefined" && delay >= 0) {
-      nextLoad = delay;
-    }
     var self = this;
     setInterval(function () {
       self.getDepartureData();
-    }, nextLoad);
+    }, this.config.updateInterval);
   },
 
-  // Call API to get departure data
+  // API aufrufen und Daten speichern
   getDepartureData: function () {
-    var url = "https://api.swu.de/mobility/v1/stop/passage/Departures?StopNumber=" + this.config.stopNumber + "&Limit=" + this.config.limit * 5;
+    var url = "https://api.swu.de/mobility/v1/stop/passage/Departures?StopNumber=" +
+      encodeURIComponent(this.config.stopNumber) +
+      "&Limit=" + encodeURIComponent(this.config.limit * 5);
+
     var self = this;
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
-      var tts = this.config.timetostop;
-      if (countdownMinutes < tts - 1) {
-        countdown.style.color = "red";
-      } else if (Math.abs(countdownMinutes - tts) <= 1) {
-        countdown.style.color = "gold";
-      } else {
-        countdown.style.color = "";
-      }
-      if (this.readyState == 4 && this.status == 200) {
-        self.departureData = JSON.parse(this.responseText).StopPassage.DepartureData;
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          try {
+            var json = JSON.parse(this.responseText);
+            self.departureData =
+              (json && json.StopPassage && json.StopPassage.DepartureData) || [];
+          } catch (e) {
+            Log.error(self.name + " - JSON parse error:", e);
+            self.departureData = [];
+          }
+        } else {
+          Log.error(self.name + " - HTTP error: " + this.status);
+        }
         self.updateDom();
       }
     };
